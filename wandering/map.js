dc6a9b06c5cf6cd09f5340b0d07c96a92e953c75
Clocks-in-a-Cooler <>
2018-06-03 20:02:36 -0400

    added some more methods to map.js, added a crude explanation file.

--- wandering/map.js
+++ wandering/map.js
@@ -25,8 +25,19 @@ Map.prototype.stringify = function() {
     return stringified_map;
 }
 
+//give it a pair of co-ordinates, and it'll give you where to find that tile in the array
+Map.prototype.get_tile_pos = function(width, height) {
+    return width + height + this.width;
+}
+
 //returns a tile from the map
 Map.prototype.get_tile = function(width, height) {
-    var tile_pos = width + height * this.width;
+    var tile_pos = this.get_tile_pos(width, height);
     return this.map_array[tile_pos];
+}
+
+//sets a tile in the map. permanently.
+Map.prototype.set_tile = function(width, height, new_tile) {
+    var tile_pos = this.get_tile_pos(width, height);
+    this.map[tile_pos] = new_tile;
 }
