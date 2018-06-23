5f93c1ab561ace6d96a762fb1a85c3b011f26707
Clocks-in-a-cooler <>
2018-06-23 16:39:57 -0400

    formatting

--- script/map.js
+++ script/map.js
@@ -31,39 +31,39 @@ Map.prototype.stringify = function() {
     }
 
     return stringified_map;
-}
+};
 
 //give it a pair of co-ordinates, and it'll give you where to find that tile in the array
 Map.prototype.get_tile_pos = function(width, height) {
     return width + height * this.width;
-}
+};
 
 //returns a tile from the map
 Map.prototype.get_tile = function(width, height) {
     var tile_pos = this.get_tile_pos(width, height);
     return this.map_array[tile_pos];
-}
+};
 
 //sets a tile in the map. permanently.
 Map.prototype.set_tile = function(width, height, new_tile) {
     var tile_pos = this.get_tile_pos(width, height);
     this.map_array[tile_pos] = new_tile;
-}
+};
 
 Map.prototype.set_special_tile = function(width, height, new_special_tile) {
     var special_tile_pos = this.get_tile_pos(width, height);
     this.special_tiles[special_tile_pos] = new_special_tile;
-}
+};
 
 Map.prototype.get_special_tile = function(width, height) {
     var special_tile_pos = this.get_tile_pos(width, height);
     return this.special_tiles[special_tile_pos];
-}
+};
 
 Map.prototype.remove_special_tile = function(width, height) {
     var special_tile_pos = this.get_tile_pos(width, height);
     this.special_tiles[special_tile_pos] = null; //set it to null
-}
+};
 
 
 //the special tile class, as a template for special tiles
@@ -77,4 +77,4 @@ function Special_tile(name, action) {
 
 Special_tile.prototype.no_action = function() {
     //do nothing
-}
+};
