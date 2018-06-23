d94c1de113c74f0871f35097e6c152ddcf8d72b6
Clocks-in-a-cooler <>
2018-06-23 16:40:58 -0400

    added map_tiles.js for tiles, and generate_map for generating maps randomly

---
+++ script/map_tiles.js
@@ -0,0 +1,16 @@
+var MAP_TILES = {
+    PLAYER = "§",
+    FARM = "ƒ",
+    OUTPOST = "R",
+    PATH = "_",
+    BRIDGE = "-",
+
+    SPECIAL = "X", //to catch the player's attention. IMMEDIATELY!!!!!!!
+
+    //various types of terrain
+    FOREST = "T",
+    MARSH = "m", //fill that in later
+    WATER = "~",
+    MOUNTAINS = "^",
+    PLAINS = "w",
+};
