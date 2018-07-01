25acc3fba6caca500dcf87c694d7af7475d5590a
Clocks-in-a-cooler <>
2018-07-01 15:58:11 -0400

    finished generator for the natural world

--- script/generate_map.js
+++ script/generate_map.js
@@ -1,3 +1,6 @@
+//WARNING! USES A S*** LOAD OF IIFES! PLEASE REFER TO THE WIKI IF YOU DO NOT UNDERSTAND THEM!
+//WARNING! VERY RESOURCE INTENSIVE! PLEASE HELP ME MAKE THIS LIGHTER ON MY COMPUTER! THANKS IN ADVANCE!
+
 function generate_natural_map(width, height) {
     if (isNaN(width)) {
         width = 75; //default value for width is 75
@@ -10,14 +13,17 @@ function generate_natural_map(width, height) {
     //create a string
     var string = "";
     for (var i = 0; i < height * width; i = i + 1) {
-        string = string + MAP_TILES.PLAINS; //for the natural map, I start with all plains
+        string = string + MAP_TILES.PLAINS; //for the natural map, I start with all plains. It's a plain map, after all (puns!)
     }
 
     var map = new Map(string, width, height); //make the map!
 
+
+    var lower_limit = Math.round((width + height) / 12);
+    var upper_limit = Math.round((width + height) / 8);
+
     //manipulate the map
-    //remember the failsafe to make sure the co-ordinates are not negative
-    var num_of_areas = random_number(3, 11); //between 3 and 11 types of terrain
+    var num_of_areas = random_number(lower_limit, upper_limit); //between 10 and 20 types of terrain
 
     while (num_of_areas > -1) {
 
@@ -28,7 +34,7 @@ function generate_natural_map(width, height) {
                 y: random_number(0, height),
             };
             var terrain_type = (function() {
-                switch (random_number(0, 3)) {
+                switch (random_number(0, 4)) {
                     case 0:
                         return MAP_TILES.FOREST;
                         break;
@@ -37,6 +43,9 @@ function generate_natural_map(width, height) {
                         break;
                     case 2:
                         return MAP_TILES.MOUNTAINS;
+                        break;
+                    case 3:
+                        return MAP_TILES.WATER;
                         break;
                     default:
                         //hopefully this won't execute
@@ -53,21 +62,28 @@ function generate_natural_map(width, height) {
             var bottom_right_x = Math.min(width,  (center.x + radius));
             var bottom_right_y = Math.min(height, (center.y + radius));
 
+            //Engine.log("setting top left at (" + top_left_x + ", " + top_left_y + "), and bottom right at (" + bottom_right_x + ", " + bottom_right_y + "). " + "terrain type: '" + terrain_type + "'.");
+
             var current_x = top_left_x;
             var current_y = top_left_y;
 
             //needs debugging
             while (current_y < bottom_right_y) {
                 while (current_x < bottom_right_x) {
-                    //if (map.get_taxicab_distance(current_x, current_y, center.x, center.y) < radius) {
+                    if (map.get_euclidean_distance(current_x, current_y, center.x, center.y) < radius) {
+                        //Engine.log("setting (" + current_x + ", " + current_y + ") as '" + terrain_type + "'.");
                         map.set_tile(current_x, current_y, terrain_type);
-                    //}
+                    }
 
                     current_x = current_x + 1;
                 }
 
+                current_x = top_left_x; //reset x. I didn't do this before; I'm a f***ing idiot!
+
                 current_y = current_y + 1;
             }
+
+            Engine.log("finished generating area with terrain type '" + terrain_type + "'.");
         })(); //I'm using an IIFE here just to spite you, ha ha
 
         num_of_areas = num_of_areas - 1; //one area down!
@@ -76,13 +92,13 @@ function generate_natural_map(width, height) {
     (function() {  //generate a river
         var point = { //start on the left side of the map
             x: 0,
-            y: random_number(0, height),
+            y: random_number(5, height - 5),
         }
 
         var next_points = [];
 
         while (point.x < width && point.y > -1 && point.y < height) {
-            Engine.log("setting (" + point.x + ", " + point.y + ") as river.");
+            Engine.log("setting (" + point.x + ", " + point.y + ") as RIVER.");
             map.set_tile(point.x, point.y, MAP_TILES.WATER);
 
             new_points = search_points(point.x, point.y, width, height).filter(function(new_point) {
